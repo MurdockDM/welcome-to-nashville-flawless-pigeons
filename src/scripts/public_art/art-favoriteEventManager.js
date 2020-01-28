0e4a6c2f0a6d2eb3828b6e552728129774867a45
Michael Clark <michaelclarknashville@protonmail.com>
2020-01-28 14:49:09 -0600

    favorite button prints text content to itinerary, used child nodes

--- src/scripts/public_art/art-favoriteEventManager.js
+++ src/scripts/public_art/art-favoriteEventManager.js
@@ -3,12 +3,16 @@ const favoriteEventHandler = (evt) => {
     const index = buttonId.split('-')[1];
   
     const artworkDiv = document.getElementById(`artwork-${index}`);
+    const childNode = artworkDiv.childNodes
+    console.log(childNode)
+    console.log(childNode[2])
+    console.log(childNode[1].textContent)
+
     const locationDiv = document.getElementById(`location-${index}`);
     const infoToSave = `${artworkDiv.textContent.trim()}: ${locationDiv.textContent.trim()}`;
     const itinerary = document.getElementById("artContainer");
-    itinerary.innerHTML = infoToSave;
 
-
+    itinerary.innerHTML = `${childNode[1].textContent} at ${childNode[3].textContent}`;
   
     const busStopSection = document.getElementById(`artwork-${index}`);
     busStopSection.classList.add('artwork_favorite');
